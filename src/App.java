5afda68649e266021dedc4eacaeb49b04c083573
dpeuan <dperezeuan@gmail.com>
2026-04-03 11:58:58 -0700

    Primer avance de ValidadorStrings, incompleto

--- src/App.java
+++ src/App.java
@@ -1,11 +1,9 @@
-import java.time.LocalDate;
-
-//import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        ValidadorFechas.esFechaFutura(LocalDate.of(2026, 6, 1));
+    
+        ValidadorStrings.estaVacio("");
+
 
     }
 }
--- src/ValidadorStrings.java
+++ src/ValidadorStrings.java
@@ -1,3 +1,66 @@
 public class ValidadorStrings {
+    //REVISADO Y FUNCIONANDO
+    public static boolean superaTamanioMaximo(String texto, int tamanioMaximo){
+        boolean resultado = true;
+        int longitud = texto.toCharArray().length;
 
+        if (tamanioMaximo >= longitud){
+            resultado = false;
+        }
+        return resultado;
+    }
+
+    //REVISADO Y FUNCIONANDO
+    public static boolean careceTamanioMinimo(String texto, int tamanioMinimo){
+        boolean resultado = true;
+        int longitud = texto.toCharArray().length;
+
+        if (tamanioMinimo <= longitud){
+            resultado = false;
+        }
+        System.out.println(resultado);
+        return resultado;
+    }
+  
+    //
+    public static boolean estaVacio(String texto){
+        boolean resultado = true;
+
+        if (texto != null){
+            resultado = false;
+        }
+        
+        System.out.println(resultado);
+        return resultado;
+    }
+
+    //
+    public static boolean contieneSoloNumeros(String texto){
+        boolean resultado = true;
+        return resultado;
+    }
+
+    //
+    public static boolean contieneSoloLetras(String texto){
+        boolean resultado = true;
+        return resultado;
+    }
+
+    //
+    public static boolean esPasswordValido(String texto){
+        boolean resultado = true;
+        return resultado;
+    }
+
+    //
+    public static boolean esPalindromo(String texto){
+        boolean resultado = true;
+        return resultado;
+    }
+
+    //
+    public static boolean esFechaValida(String texto){
+        boolean resultado = true;
+        return resultado;
+    }
 }
